760dd6bca5fd2bed76e4e613a915191341b5b853
chuguangming <chu888chu888@qq.com>
2014-06-20 17:20:01 +0800

    20140620

---
+++ 1-Table/1-基本的表格生成SimpleTable3/SimpleTable3/SimpleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SimpleTableViewController.h
+//  SimpleTable3
+//
+//  Created by Rickie Li on 12-10-11.
+//  Copyright (c) 2012年 EntLib.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleTableViewController : UIViewController<UITableViewDelegate, UITableViewDataSource>
+
+@end
