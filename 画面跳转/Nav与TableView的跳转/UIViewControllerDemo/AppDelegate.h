52fbe0a64129a148a30c67fda928a83e0f8564f7
chuguangming <chu888chu888@qq.com>
2014-08-06 17:34:24 +0800

    20140803

---
+++ 画面跳转/Nav与TableView的跳转/UIViewControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewControllerDemo
+//
+//  Created by chuguangming on 14-8-4.
+//  Copyright (c) 2014年 chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong,nonatomic) UIViewController *viewController1;
+@property (strong,nonatomic) UIViewController *viewController2;
+@property (strong,nonatomic) UINavigationController *rootController;
+@end
